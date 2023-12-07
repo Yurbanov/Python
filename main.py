2bd40dac54298379181be3cc1efe642ce994bfa3
yusif gurbanov <yusif.gurbanov@gmail.com>
2023-12-07 03:33:18 -0800

    Second file

--- main.py
+++ main.py
@@ -1,1 +1,91 @@
 print("New Project")
+
+#This is introduction point to comment
+
+""" Multiple line comment"""
+"""a=10
+print(a)
+
+total = (1+39)/2
+print(total)
+
+#qoutes in python
+
+var = 'Data'
+print(var)
+
+var ="programming"
+print(var) """
+
+var = """New Project"""
+print (var)
+
+"""a = input("Some number:")
+print(a)
+
+a=input("Enter Name:")
+print(a)
+
+a = 1; b = 3; c=5; print(a,b,c) """
+
+
+a = 1; b = 3
+a,b = b,a
+print(a,b)
+
+x = 15
+y = 40
+print (x * y)
+print (x - y)
+print ( x % y)
+
+x = 15
+print(type(x))
+
+x = 18.48
+print(type(x))
+
+x = 16J
+print(type(x))
+ 
+
+num = 28>16
+print(type(num))
+print(num)
+
+name = "Leo"
+print(len(name))
+
+print(name[2])
+print(name[1:4])
+print(name.upper())
+
+mylist = [10,20,30, "data", "program"]
+print(mylist[2:5])
+mylist[2]=50
+print(mylist)
+
+mylist.append(10)
+print(mylist)
+ 
+mylist.insert(5,130)
+print(mylist)
+
+animals = {'reptiles':'snake', 'mammals':'whale', 'amphibian':'frog'}
+print(animals['amphibian'])
+animals['three']='Dog'
+animals['four']="Cat"
+print(animals)
+
+
+myset = {10,30,23,30,58}
+print(myset)
+
+print(list(range(11)))
+
+a = [1,3,4,6]
+b = [7,8,9,5]
+c=[a,b]
+print(c)
+
+
